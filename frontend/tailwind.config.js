30ecb677a2906bcae267de7ecc861255feb51462
MrLad01 <ladayodeji@gmail.com>
2023-11-05 17:16:19 +0100

    update

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/App.tsx'
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs' : '325px'
+      }
+    },
+  },
+  plugins: [],
+}
+
