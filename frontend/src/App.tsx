eb5473221937b3a8f940d2245721d65e50e2c061
MrLad01 <ladayodeji@gmail.com>
2023-11-05 18:03:07 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,10 @@ import Nav from "./components/Nav"
 function App() {
 
   return (
-    <main className = "w-full h-screen" >
-      <Nav />
+    <main className = "w-full h-screen md:max-2xl:pl-36 md:max-2xl:pr-32 md:max-2xl:pt-2 md:max-2xl:pb-10 " >
+      <div className = "w-full h-full" >
+        <Nav />
+      </div>
     </main>
   )
 }
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+import cart from "../assets/images/icon-cart.svg";
+import avatar from "../assets/images/image-avatar.png";
+
 
 const Nav:React.FC = () => {
   return (
-    <>
-      
-    </>
+    <nav className = ' flex items-center justify-between ' >
+      <div className = 'flex' >
+        <header>
+            <h1> sneakers </h1>
+        </header>
+        
+        <div className = ' flex ' >
+            <a href = "/" > Collections </a>
+            <a href = "/" > Men </a>
+            <a href = "/" > Women </a>
+            <a href = "/" > About </a>
+            <a href = "/" > Contact </a>   
+        </div>
+      </div>
+
+      <div className = 'flex items-center ' >
+        <div> <img src = { cart } alt="" /> </div>
+        <div> <img src = { avatar }  alt="" /> </div>
+      </div>
+    </nav>
   )
 }
 
